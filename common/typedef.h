76b9301b0cc59e76df41ab7ec7a4058f13649991
Jeff Epler <jepler@gmail.com>
2023-06-27 15:22:22 -0500

    it buildy

---
+++ common/typedef.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+typedef int16_t Word16;
+typedef int32_t Word32;
+typedef uint16_t UWord16;
+typedef uint32_t UWord32;
+
+typedef _Bool Flag;
+
+static void logic16(void) {}
+static void logic32(void) {}
+static void move16(void) {}
+static void move32(void) {}
+static void test(void) {}
+
